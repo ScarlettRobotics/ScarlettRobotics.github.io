8246959e97dd7242e50a0f0b5ae293f5fa86bbbf
Avery Keuben <avery1516@gmail.com>
2021-11-16 23:02:59 -0700

    Fix static files

--- next.config.js
+++ next.config.js
@@ -2,9 +2,3 @@
 module.exports = {
   reactStrictMode: true,
 }
-
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/ScarlettRobotics.github.io/' : ''
-}
