cf57c3dc7b30755e3e1ac08b65aab26ba20b7d62
Avery Keuben <avery1516@gmail.com>
2021-11-16 22:24:41 -0700

    Prepare project for github pages

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/ScarlettRobotics.github.io/' : ''
+}
